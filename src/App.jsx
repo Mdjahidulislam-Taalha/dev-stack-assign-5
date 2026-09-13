8d2ca5639ffdab5af619e4f31d2f09bdd0e4364c
Taalha <jahidulislamtaalha@gmail.com>
2026-09-14 04:00:01 +0600

    Integrate React-Toastify notifications and setup App layout

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import TechnologySection from "./components/TechnologySection";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Hero />
+      <TechnologySection />
+    </>
+  );
+}
+
+export default App;
